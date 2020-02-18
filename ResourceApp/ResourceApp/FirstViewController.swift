19bc43cacd4b2c5f8a296bead3d9f06dfd2f4da0
Roel Spruit <roelspruit@gmail.com>
2020-02-18 08:34:19 +0100

    add entitlements to testproject

--- ResourceApp/ResourceApp/FirstViewController.swift
+++ ResourceApp/ResourceApp/FirstViewController.swift
@@ -14,6 +14,7 @@ class FirstViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    print(R.entitlements.comAppleDeveloperSiri)
     // Do any additional setup after loading the view, typically from a nib.
     titleLabel.font = R.font.averiaLibreBoldItalic(size: 36)
   }
